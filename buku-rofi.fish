1a6c18e3ed7e24bd333a80aa8206ea07af095bcd
SPREEKDOS <72156842+SPREEKDOS@users.noreply.github.com>
2025-07-06 00:00:43 +0300

    Refactoring code and adding functions

--- buku-rofi.fish
+++ buku-rofi.fish
@@ -2,25 +2,74 @@
 # @SPREEKDOS/buku-rofi/buku-rofi.fish:v1.0.0
 #TODO add tags searching
 #TODO remember last prompt before switching between functions?
-#set fish_trace 1
+#TODO Copy URLs or titles to clipboard
+#TODO use ai to generate tags when adding bookmark
+#TODO strip trailing slash
+set -g fish_trace 1
+# Function to display help
+function display_help
+    echo "Usage: $script_name [options]"
+    echo "Options:"
+    echo "  -d, --debug   Enable debug mode"
+    echo "  -h, --help    Display this help message"
+    exit 0
+end
 
-set -g script_name $(status basename)
+# Function to handle arguments
+function handle_arguments
+    argparse --name "$script_name" d/debug h/help -- $argv
+    if set -q _flag_d
+        set -g fish_trace 1
+    end
+    if set -q _flag_h
+        display_help
+    end
+    if set -q _flag_e
+        buku-export
+    end
+    if set -q _flag_i
+        buku-import
+    end
+end
 
-if not which -s buku >/dev/null
-    echo "$script_name: cant find buku, to install it use: sudo apt install buku"
-    exit 1
+# Function to check if a command exists
+function check_command
+    for cmd in $argv
+        if not command -s $cmd >/dev/null
+            echo "$script_name: Command '$cmd' not found. To install it, use: sudo apt install $cmd"
+            exit 1
+        end
+    end
 end
-if not which -s rofi >/dev/null
-    echo "$script_name: cant find rofi, to install it use: sudo apt install rofi"
-    exit 1
+
+# Function to initialize script settings
+function init_script
+    set -g script_name (status basename)
+    set -g disable_defined_custom_keybind 'configuration { kb-custom-1 : "" ;
+    kb-custom-2 : "" ; 
+    kb-custom-3 : "" ; 
+    kb-custom-4 : "" ; 
+    kb-custom-5 : "" ; 
+    kb-custom-6 : "" ;
+    }'
+    set -g matching_buku normal
+    set -g auto_filter ""
 end
-if not which -s notify-send >/dev/null
-    echo "$script_name: cant find notify-send, to install it use: sudo apt install notify-send"
-    exit 1
+
+# Function to export bookmarks to a file
+function buku-export
+    buku -e $export_file
+    echo "$script_name: Bookmarks exported to $export_file"
 end
-if not which -s awk >/dev/null
-    echo "$script_name: cant find awk, to install it use: sudo apt install awk"
-    exit 1
+
+# Function to import bookmarks from a file
+function buku-import
+    if test -f $import_file
+        buku -i $import_file
+        error_rofi "Bookmarks imported from $import_file"
+    else
+        error-rofi "$import_file not found."
+    end
 end
 
 function buku
@@ -32,8 +81,10 @@ end
 function notify-send
     command notify-send --app-name=$script_name --urgency=low --expire-time=2500 $argv
 end
+# Function to dsiplay confirm dialog in rofi
 function confirm-rofi
-    set confirm_rofi_answer $(echo Yes\nCancel | rofi -theme-str "mainbox { children : [textbox-confirm, listview] ;}
+    set confirm_rofi_answer $(echo Yes\nCancel | rofi -theme-str "$disable_defined_custom_keybind
+    mainbox { children : [textbox-confirm, listview] ;}
     textbox-confirm {content : \"$script_name | $argv \"; }
     listview { layout :  horizontal ;
         spacing : 15 ;}")
@@ -45,8 +96,10 @@ function confirm-rofi
             return 1
     end
 end
+# Function to dsiplay error dialog in rofi
 function error-rofi
-    set -l error_rofi_answer $(echo Retry\nReturn | rofi -theme-str "mainbox { 
+    set -l error_rofi_answer $(echo Retry\nReturn | rofi -theme-str "$disable_defined_custom_keybind
+    mainbox { 
     children : [ textbox-error, listview ] ;}
         textbox-error { content : \"$script_name | $argv[1] : \" ;}
         listview { layout :  horizontal ;
@@ -60,9 +113,11 @@ function error-rofi
     end
     exit 1
 end
-function search-buku
+# Function to open a bookmark in the defualt browser
+function open-buku
     buku --open $rofi_output[1]
 end
+# Function to add a bookmark
 function add-buku
     if not test -z "$argv"
         set bookmark_url $(string replace ' ' '' $(string match --regex '^.*?\s+' "$argv" ))
@@ -81,13 +136,15 @@ $(test -z "$bookmark_title"; and echo Title : $bookmark_title)"
         return 0
     end
 
-    set bookmark_url $(string replace ' ' '' $(command rofi -dmenu -replace -i -no-click-to-exit -theme "buku-rofi.rasi" -p "$script_name | Enter URL of the new bookmark" -format f -theme-str "mainbox { children : [Inputbar] ;}
+    set bookmark_url $(string replace ' ' '' $(command rofi -dmenu -replace -i -no-click-to-exit -theme "buku-rofi.rasi" -p "$script_name | Enter URL of the new bookmark" -format f -theme-str "$disable_defined_custom_keybind
+    mainbox { children : [Inputbar] ;}
 entry { placeholder : 'URL' ; }"))
     if test -z "$bookmark_url"
         main
     end
     set bookmark_url "$(string replace --regex https?:// '' $bookmark_url)"
-    set second_prompt $(echo -e "Save\0permanent\x1ftrue\nAdd title\0permanent\x1ftrue" | command rofi -dmenu -format s\nf -replace -i -no-click-to-exit -theme buku-rofi.rasi -p "$script_name | Enter tags of the new bookmark" -theme-str "mainbox { children : [Inputbar, listview] ;} entry { placeholder : 'tag1, tag2, tag3, ...' ; }
+    set second_prompt $(echo -e "Save\0permanent\x1ftrue\nAdd title\0permanent\x1ftrue" | command rofi -dmenu -format s\nf -replace -i -no-click-to-exit -theme buku-rofi.rasi -p "$script_name | Enter tags of the new bookmark" -theme-str "$disable_defined_custom_keybind
+    mainbox { children : [Inputbar, listview] ;} entry { placeholder : 'tag1, tag2, tag3, ...' ; }
 listview { layout :  horizontal ;
         spacing : 15 ;}")
     set bookmark_tags "$second_prompt[2]"
@@ -96,7 +153,8 @@ listview { layout :  horizontal ;
     end
     switch $second_prompt[1]
         case 'Add title'
-            set bookmark_title $(command rofi -dmenu -replace -i -no-click-to-exit -theme "buku-rofi.rasi" -p "$script_name | Enter title of the new bookmark" -format f -theme-str "mainbox { children : [Inputbar] ;}
+            set bookmark_title $(command rofi -dmenu -replace -i -no-click-to-exit -theme "buku-rofi.rasi" -p "$script_name | Enter title of the new bookmark" -format f -theme-str "$disable_defined_custom_keybind
+            mainbox { children : [Inputbar] ;}
         entry { placeholder : 'Title' ; }")
     end
     if test -z "$bookmark_title"
@@ -106,25 +164,29 @@ listview { layout :  horizontal ;
     end
     or error-rofi "buku exited with error $status"
 
-    #    notify-send "$script_name: Added bookmark to buku" "Bookmark details
-    #URL : $bookmark_url
-    #tags : $bookmark_tags
-    #$(test -z "$bookmark_title"; and echo Title : $bookmark_title)"
+    notify-send "$script_name: Added bookmark to buku" "Bookmark details
+    URL : $bookmark_url
+    tags : $bookmark_tags
+    $(test -z "$bookmark_title"; and echo Title : $bookmark_title)"
 
 end
 
+# Function to delete a bookmark
 function delete-buku
     if confirm-rofi "Ary you sure you want to delete : $rofi_output[2]"
         echo y | command buku --nostdin --np --nc --delete $rofi_output[1]
     end
 end
+
+# Function to edit an existing bookmark
 function edit-buku
-    set edited_bookmark $(echo Save\nCancel | command rofi -dmenu -replace -i -no-click-to-exit -theme "buku-rofi.rasi" -p "$script_name | Edit the bookmark" -format s\nf -filter "$rofi_output[2]" -theme-str "mainbox { children : [Inputbar] ;}
-entry { placeholder : 'New bookmark' ; }")
-    switch edited_bookmark
-        case Cancel
-            main
-        case ''
+    set edited_bookmark $(echo -e "Save\0permanent\x1ftrue\nCancel\0permanent\x1ftrue" | command rofi -dmenu -replace -i -no-click-to-exit -theme "buku-rofi.rasi" -p "$script_name | Edit the bookmark" -format s\nf -filter "$rofi_output[2]" -theme-str "$disable_defined_custom_keybind
+    mainbox { children : [Inputbar,listview] ;}
+    entry { placeholder : 'New bookmark' ; }
+    listview { layout : horizontal ;
+    spacing : 15 ;}")
+    switch $edited_bookmark
+        case Cancel ''
             main
     end
     set bookmark_url $(string replace ' ' '' $(string match --regex '^.*?\s+' "$edited_bookmark" ))
@@ -149,22 +211,40 @@ function hide_results
     end
 end
 function matching-buku
-    set -g matching_buku $(echo normal\nregex\nglob\nfuzzy\nprefix | rofi -p "Choose matching type" -theme-str "mainbox { children : [Inputbar, listview] ;}
+    set -g matching_buku $(echo normal\nregex\nglob\nfuzzy\nprefix | rofi -p "$script_name | Choose matching type" -theme-str "$disable_defined_custom_keybind
+    mainbox { children : [Inputbar, listview] ;}
 Inputbar {children : [prompt, textbox-prompt-sep]; }
 listview { layout : horizontal ;
 spacing : 15 ;}")
 
 end
+function auto-filter
+    set saved_tags "$(cat saved-tags)"
+    set -g auto_filter $(echo -e "$(if test -n "$saved_tags" ; echo "$saved_tags\n" ; end)Save tags from search query\0permanent\x1ftrue" | rofi -p "$script_name | Auto-filter" -format s\nf -theme-str "$disable_defined_custom_keybind
+        mainbox { children : [Inputbar, listview] ;}
+        Inputbar { children : [prompt, textbox-prompt-sep, entry]; }")
+    switch $auto_filter[1]
+        case "Save tags to the list"
+            if not contains "$auto_filter[2]" $(cat saved-tags)
+                echo $auto_filter[2] >> saved-tags
+            end
+            set -g auto_filter[1] $auto_filter[2]
+            set -ge auto_filter[2..]
+    end
+end
+
+# Function to main loop
 function main
     argparse --name $script_name h/help -- $argv
     while true
         if not set -q matching_buku
             set -g matching_buku normal
         end
+        set bookmarks $(buku --format 4 --print | awk -F ' ' '{print "[ " $1 " ]   " $2 "   " $NF }')
         if not set -q hide_results
-            set -g rofi_output $(echo -e "$(buku --format 4 --print | awk -F ' ' '{print "[ " $1 " ]   " $2 "   " $NF }' )\nAdd search quary as bookmark\0permanent\x1ftrue" | rofi -p "$script_name" -format d\ns\nf -matching "$matching_buku" -theme-str "textbox-matching { content : \"$matching_buku\" ;  }")
+            set -g rofi_output $(echo -e "$(if test -n "$bookmarks" ; echo $bookmarks"\n" ; end )Add search quary as bookmark\0permanent\x1ftrue" | rofi -p "$script_name" -format d\ns\nf -matching "$matching_buku" -filter "$auto_filter" -theme-str "textbox-matching { content : \"$matching_buku\" ;  }" )
         else
-            set -g rofi_output $(echo -e "$(buku --format 4 --print | awk -F ' ' '{print "[ " $1 " ]   " $2 "   " $NF }' )\nAdd search quary as bookmark\0permanent\x1ftrue" | rofi -format d\ns\nf -matching "$matching_buku" -theme-str "mainbox { children : [box-hint, Inputbar, box-hint2] ; }
+            set -g rofi_output $(echo -e "$(if test -n "$bookmarks" ; echo $bookmarks"\n" ; end)Add search quary as bookmark\0permanent\x1ftrue" | rofi -format d\ns\nf -matching "$matching_buku" -filter "$auto_filter" -theme-str "mainbox { children : [box-hint, Inputbar, box-hint2] ; }
             box-hint2 { children : [textbox-hint-listview] ;
                 orientation :  horizontal ;
                 border : 1 0 0 0; }
@@ -180,10 +260,12 @@ function main
                     case "Add search quary as bookmark"
                         add-buku "$rofi_output[3]"
                     case '*'
-                        search-buku
+                        open-buku
                         and return 0
                 end
             case 1
+                exit 0
+            case 65
                 exit 1
             case 10
                 matching-buku
@@ -195,9 +277,20 @@ function main
                 edit-buku
             case 14
                 hide_results
+            case 15
+                auto-filter
+                main
+        end
+        if set -q auto_filter
+            set -ge auto_filter
         end
     end
 end
-
+init_script $argv
+check_command buku rofi notify-send awk 
+handle_arguments $argv
 main $argv
-#set -e fish_trace
+
+if set -q _flag_d
+    set -e fish_trace
+end
